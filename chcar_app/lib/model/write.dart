94df079f6d2fa819f91a04282c872d887c5c5ebb
hwicoding <;1029>
2024-04-19 23:31:43 +0900

    create write model and update firebase in detail_page

---
+++ chcar_app/lib/model/write.dart
@@ -0,0 +1,23 @@
+class Write {
+  int seq;
+  String id;
+  String carSeq;
+  String carInfo;
+  String sellPrice;
+  String lat;
+  String lng;
+  String img;
+  int state;
+
+  Write({
+    required this.seq,
+    required this.id,
+    required this.carSeq,
+    required this.carInfo,
+    required this.sellPrice,
+    required this.lat,
+    required this.lng,
+    required this.img,
+    required this.state,
+  });
+}
--- chcar_app/lib/view/detail_page.dart
+++ chcar_app/lib/view/detail_page.dart
@@ -1,5 +1,6 @@
+import 'package:chcar_app/model/write.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import 'predict_page.dart';
@@ -27,288 +28,22 @@ class _DetailPageState extends State<DetailPage> {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Center(
-          child: Column(
-            children: [
-              // 사진
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 30, 30, 5),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  // ignore: avoid_unnecessary_containers
-                  child: Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'images/test_01.webp',
-                          width: 300,
-                        ),
-                        Image.asset(
-                          'images/test_02.webp',
-                          width: 300,
-                        ),
-                        Image.asset(
-                          'images/test_03.webp',
-                          width: 300,
-                        ),
-                        Image.asset(
-                          'images/test_04.webp',
-                          width: 300,
-                        ),
-                        Image.asset(
-                          'images/test_05.webp',
-                          width: 300,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              // 가격
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '가격 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '20,000 €',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 브랜드
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '브랜드 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Hyundai',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 모델
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '모델 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Kona',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 생산연도
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '생산연도 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '2017',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 색상
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '색상 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Black',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 주행거리
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '주행거리 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '20,000km',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 연료유형
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '연료유형 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '가솔린',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 변속기유형
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '연료유형 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '오토',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 차량연비
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '차량연비 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '18.3',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // 마력
-              const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Row(
-                  children: [
-                    Text(
-                      '마력 : ',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '4.5',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 0, 30, 15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
-                      child: Row(
-                        children: [
-                          Text(
-                            '설명',
-                            style: TextStyle(
-                              fontSize: 26,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 370,
-                      height: 500,
-                      child: TextField(
-                        maxLines: 300,
-                        controller: infoController,
-                        decoration: const InputDecoration(
-                          labelText: '설명',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('write')
+                .orderBy('seq', descending: false)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final documents = snapshot.data!.docs;
+              return ListView(
+                children: documents.map((e) => _buildItemWidget(e)).toList(),
+              );
+            },
           ),
         ),
       ),
@@ -352,4 +87,323 @@ class _DetailPageState extends State<DetailPage> {
       ),
     );
   }
-}
+
+  // --- Function ---
+  Widget _buildItemWidget(doc) {
+    final write = Write(
+      seq: doc['seq'],
+      id: doc['id'],
+      carSeq: doc['carseq'],
+      carInfo: doc['carinfo'],
+      sellPrice: doc['sellprice'],
+      lat: doc['lat'],
+      lng: doc['lng'],
+      img: doc['img'],
+      state: doc['state'],
+    );
+    return Column(
+      children: [
+        // 사진
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 30, 30, 5),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            // ignore: avoid_unnecessary_containers
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'images/test_01.webp',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'images/test_02.webp',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'images/test_03.webp',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'images/test_04.webp',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'images/test_05.webp',
+                    width: 300,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        // 가격
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '가격 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '20,000 €',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 판매자
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '판매자 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Hwicoding',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 브랜드
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '브랜드 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Hyundai',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 모델
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '모델 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Kona',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 생산연도
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '생산연도 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '2017',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 색상
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '색상 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Black',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 주행거리
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '주행거리 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '20,000km',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 연료유형
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '연료유형 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '가솔린',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 변속기유형
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '연료유형 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '오토',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 차량연비
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '차량연비 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '18.3',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 마력
+        const Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Row(
+            children: [
+              Text(
+                '마력 : ',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '4.5',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                child: Row(
+                  children: [
+                    Text(
+                      '설명',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 370,
+                height: 500,
+                child: TextField(
+                  maxLines: 300,
+                  controller: infoController,
+                  decoration: const InputDecoration(
+                    labelText: '설명',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+} // End
