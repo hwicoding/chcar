f820fb1267bd530f53091f9bfba1d52aa1d0ac3b
hwicoding <;1029>
2024-04-19 11:02:32 +0900

    24.04.19 Murge and Add detailpage.

---
+++ chcar_app/lib/view/detail_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'predict_page.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'images/logo.png',
+          width: 150,
+        ),
+        backgroundColor: Color.fromARGB(255, 79, 139, 116),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('내 차 시세 예측'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('예측 기록 보기'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('내 차 판매 글'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('공지사항'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromARGB(255, 79, 139, 116),
+        onPressed: () {
+          Get.to(const PredictPage());
+        },
+        child: Center(
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
